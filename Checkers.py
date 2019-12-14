f2ffaf89108d4a6a64a1a1ba9761662f7b5e71f4
Jakub <jakub90stanczak@gmail.com>
2019-12-14 17:23:27 +0100

    Change to nested classes

--- Checkers.py
+++ Checkers.py
@@ -15,7 +15,7 @@ class Square:
         self.y = y
         self.sq_black = black
         self.selected = False
-        self.peace = None
+        self.piece = None
 
     def draw(self):
         color_sq = (96, 96, 96) if self.sq_black else (255, 255, 255)
@@ -23,7 +23,7 @@ class Square:
         pygame.draw.rect(win, color, (self.x * self.dim, self.y * self.dim + offset, self.dim , self.dim))
 
     def __repr__(self):
-        return "{} {} {}".format(self.x, self.y, self.sq_black)
+        return "{} {} {}".format(self.x, self.y, self.piece)
 
 
 class Piece:
@@ -39,6 +39,11 @@ class Piece:
         color = (0, 0, 0) if self.black else (224, 224, 224)
         pygame.draw.circle(win, color, (self.x * Square.dim + Square.dim // 2, self.y * Square.dim + Square.dim // 2 + offset), self.rad)
 
+    def __repr__(self):
+        if self.black:
+            return "Black piece"
+        else:
+            return "White piece"
 
 board_width = 8
 board_height = 8
@@ -50,33 +55,31 @@ win = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Checkers by Kuba")
 
 board = []
-pieces_w = []
-pieces_b = []
 def init():
     for x in range(board_width):
         column = []
         for y in range(board_height):
             column.append(Square(x, y, True if (x + y) % 2 == 1 else False))
         board.append(column)
-    print(board[1][5])
 
-    for x in range(8):
-        for y in range(3):
-            pieces_w.append(Piece(x, y, False))
-        for y in range(5,8):
-            pieces_b.append(Piece(x, y, True))
+    # for x in range(8):
+    #     for y in range(3):
+    #         board[x][y].piece = Piece(x, y, False)
+    #     for y in range(5, 8):
+    #         board[x][y].piece = Piece(x, y, True)
 
 
 def draw():
     win.fill((255, 255, 255))
-    for column in board:
-        for sq in column:
-            sq.draw()
-    for piece in pieces_b + pieces_w:
-        piece.draw()
+    for sq in chain.from_iterable(board):
+        sq.draw()
+        if sq.piece is not None:
+            sq.piece.draw()
     pygame.display.update()
 
 def select_sq(x, y):
+    print()
+    print("square selected")
     if y < offset:
         return
     x = x // Square.dim
@@ -92,18 +95,21 @@ def select_sq(x, y):
         if sq.selected:
             num_selected += 1
             start_pos = sq
-    if num_selected == 0:
+    if num_selected == 0 and board[x][y].piece is not None:
         board[x][y].selected = True
         return
     else:
         for sq in chain.from_iterable(board):
             sq.selected = False
+        move_piece(start_pos, board[x][y])
 
 
-def move_piece(start_pos):
-    pass
-
-
+def move_piece(start_pos, end_pos):
+    print(start_pos)
+    print(end_pos)
+    #  check if move is legal
+    end_pos.piece = start_pos.piece
+    start_pos.piece = None
 
 
 
